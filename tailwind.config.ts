0013b4f7b4994f30364db860899835ea245277b2
Andrea Joselin Hernandez Lopez <andrehern@ar0c02dj1wwml85.ml.com>
2023-08-30 20:44:28 -0300

    initial config

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        shimmer: {
+          '100%': {
+            transform: 'translateX(100%)',
+          },
+        },
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      minHeight: {
+        'inherit': 'inherit'
+      },
+      height: {
+        '600': '600px',
+        '800': '800px',
+      }
+    },
+  },
+  plugins: [],
+}
+export default config
