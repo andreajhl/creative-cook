13be9ec0096630faefa242ecf7e5fc6f9d1533a7
Andrea Hernandez <andreajhl29@gmail.com>
2023-08-31 16:49:56 -0300

    fix id category

--- src/constants/categories/index.ts
+++ src/constants/categories/index.ts
@@ -5,5 +5,5 @@ const { home: { categories: { breakfast, brunch, snack } } } = wordings;
 export const categories = [
   { url: '/images/category/breakfast.jpeg', title: breakfast, id: 'breakfast' },
   { url: '/images/category/brunch.jpeg', title: brunch, id: 'brunch' },
-  { url: '/images/category/snack.jpeg', title: snack, id: 'snateatimeck' }
+  { url: '/images/category/snack.jpeg', title: snack, id: 'teatime' }
 ];
