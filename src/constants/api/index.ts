9d8c780d3e0a76964d06e932bdb44f26bf63a8ce
Andrea Hernandez <andreajhl29@gmail.com>
2023-08-31 05:02:19 -0300

    remove constants

--- src/constants/api/index.ts
+++ src/constants/api/index.ts
@@ -2,8 +2,6 @@ import 'dotenv/config';
 
 const { APP_KEY, APP_ID, TYPE } = process.env;
 
-export const LIMIT_ITEMS = 4;
-export const VISIBLE_PAGES = 4;
 export const basicParams = {
   app_key: APP_KEY,
   app_id: APP_ID,
