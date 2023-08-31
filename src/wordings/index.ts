fb0d698499dff9950c251bfddd49afd40a941280
Andrea Hernandez <andreajhl29@gmail.com>
2023-08-31 04:52:35 -0300

    add wordings

---
+++ src/wordings/index.ts
@@ -0,0 +1,54 @@
+export default ({
+  title: 'Cocina Creativa',
+  navbar: {
+    placeholder: '¿Que quieres preparar hoy?',
+  },
+  product: {
+    calories: 'Calorias',
+    ingredients: 'Ingredientes',
+    infoNutritional: 'Info Nutricional',
+    portion: 'Prciones:'
+  },
+  carousel: {
+    next: 'Next',
+    prev: 'Previous'
+  },
+  nav: {
+    menu: 'Menu',
+    close: 'Cerrar'
+  },
+  nutritionTable: [
+    'Contenido energetico',
+    'Valor por porción'
+  ],
+  home: {
+    banner: {
+      health: {
+        title: '+Salud',
+        subtitle: 'Recetas sanas y faciles'
+      },
+      table: {
+        title: 'Nutricion',
+        subtitle: 'Tabla nutricional por porcion'
+      },
+      recipes: {
+        title: '+60',
+        subtitle: 'Recetas variadas'
+      }
+    },
+    categories: {
+      breakfast: 'Desayunos',
+      brunch: 'Almuerzos',
+      snack: 'Meriendas'
+    }
+  },
+  warnings: {
+    createOrden: 'Error no controlado, hable con el administrador',
+    loading: 'Cargando...',
+    pageNotFound: {
+      title: 'Oops!',
+      subtitle: 'Lo sentimos. No encontramos la página',
+      retry: 'Regresar al Home'
+    }
+  }
+});
