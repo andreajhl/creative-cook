efdddd0a74842bd5c4e668b8baf7abeafa5f01cc
Andrea Hernandez <andreajhl29@gmail.com>
2023-08-31 04:56:50 -0300

    add mocks

---
+++ src/mocks/recipes/index.ts
@@ -0,0 +1,37 @@
+export const RECIPE_NORMALIZE = {
+  id: "23fe961249ccc3d59a62bced57b1b2b9",
+  title: "Green Bean and Plum Salad",
+  image: "http://http2.mlstatic.com/test.jpg",
+  portions: 8,
+  tags: [
+    "High-Fiber",
+    "Low-Fat",
+  ],
+  nutrients: [
+    {
+      title: "Protein",
+      value: '7 g',
+    }
+  ],
+  ingredients: [
+    "1/2 pound green beans",
+    "10 small plums",
+  ],
+}
+
+export const RECIPE_LIST_NORMALIZE = [
+  {
+    id: "23fe961249ccc3d59a62bced57b1b2b9",
+    title: "Green Bean and Plum Salad",
+    image: "http://http2.mlstatic.com/test.jpg",
+    calories: '190',
+    quantityIngredients: 5,
+  },
+  {
+    id: "23fe961249cac2d49a62bced57b1b2b9",
+    title: "Cold Brew Iced Coffee Recipe",
+    image: "http://http2.mlstatic.com/test.jpg",
+    calories: '110',
+    quantityIngredients: 3,
+  }
+];
